3aecce265008d215361e462146186a56943b1248
pedroleyva0 <pedro.leyva.molina@gmail.com>
2023-01-16 12:38:16 +0100

    creacion hello world

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿...
+  private void Execute(object sender, EventArgs e)
+{
+    ThreadHelper.ThrowIfNotOnUIThread();
+    string message = "Hello World!";
+    string title = "Command";
+
+    // Show a message box to prove we were here
+    VsShellUtilities.ShowMessageBox(
+        this.ServiceProvider,
+        message,
+        title,
+        OLEMSGICON.OLEMSGICON_INFO,
+        OLEMSGBUTTON.OLEMSGBUTTON_OK,
+        OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+}
+...
